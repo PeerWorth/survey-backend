8793994cc2cfdbfd9251f78a80dad40140d7f872
kcw2297 <kcw2371@gmail.com>
2025-05-22 11:11:32 +0900

    feat: 엑셀 파일 load/save 함수 모음 추가

---
+++ app/data/excel.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def create_excel(data: list, path: str, columns: list[str]) -> None:
+    df = pd.DataFrame(data, columns)
+    df.to_excel(path, index=False)
+    return
+
+
+def load_excel(path: str) -> list[tuple]:
+    df = pd.read_excel(path, engine='openpyxl')
+    return [tuple(row) for row in df.itertuples(index=False, name=None)]
+   
