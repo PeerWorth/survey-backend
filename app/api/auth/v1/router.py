fa47d8af37b820675bd6db85565b9f4e55ba5b9b
kcw2297 <kcw2371@gmail.com>
2025-08-17 16:24:32 +0900

    fix: api response post/get status_code 수정

--- app/api/auth/v1/router.py
+++ app/api/auth/v1/router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, status
 
 from app.api.auth.v1.schemas.user_schema import UserEmailRequest
 from app.common.docs.responses import COMMON_ERROR_RESPONSES
-from app.common.schemas.base_schema import SuccessResponse
+from app.common.schemas.base_schema import SuccessPostResponse
 from app.module.auth.response import AUTH_ERROR_RESPONSES
 from app.module.auth.services.user_service import UserService
 
@@ -12,7 +12,8 @@ auth_router = APIRouter(prefix="/v1")
 @auth_router.post(
     "/email",
     summary="유저 이메일과 동의 약관 저장",
-    response_model=SuccessResponse,
+    status_code=status.HTTP_201_CREATED,
+    response_model=SuccessPostResponse,
     responses={
         **COMMON_ERROR_RESPONSES,
         **AUTH_ERROR_RESPONSES,
@@ -21,6 +22,6 @@ auth_router = APIRouter(prefix="/v1")
 async def submit_user_email(
     request_data: UserEmailRequest,
     user_service: UserService = Depends(),
-) -> SuccessResponse:
+) -> SuccessPostResponse:
     await user_service.save_user_with_marketing(request_data)
-    return SuccessResponse(code=status.HTTP_201_CREATED)
+    return SuccessPostResponse()
